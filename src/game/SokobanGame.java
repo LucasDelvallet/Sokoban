5ebdfdbef16a6bb805178f24f67af9de816d58a2
MickaelAlvarez <alvarez.mickael.93@gmail.com>
2015-01-23 15:33:39 +0100

    Menu
    Adaptation of the nwe menu from gramework.
    Add the dynamique evolution of level in game.
    Add a nwe level.

--- src/game/SokobanGame.java
+++ src/game/SokobanGame.java
@@ -3,37 +3,52 @@ package game;
 import gameframework.game.Game;
 import gameframework.game.GameData;
 import gameframework.game.GameLevel;
-
+import gameframework.game.GameLevelDefaultImpl;
+import gameframework.gui.GameStatusBarElement;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-import level.SokobanLevel;
-
 /**
- * Create a basic game application, a menu and displays the current level.
- * 
- * @author NINTUNZE, DOUBLET, DELVALLET Q, DELVALLET L, ALVAREZ
+ * Create a basic game application with menus and displays of lives and score
  */
 public class SokobanGame implements Game, Observer {
 
-	private SokobanLevel currentPlayedLevel = null;
+	private GameLevelDefaultImpl currentPlayedLevel = null;
 
 	protected final GameData data;
+	protected final ArrayList<GameStatusBarElement<Integer>> windowElements;
 
-	public SokobanGame(GameData data) {
-		this.data = data;
+	
+	@SafeVarargs
+	public SokobanGame(GameData gamedata,
+			GameStatusBarElement<Integer>... elements) {
+		this.data = gamedata;
 		data.getEndOfGame().addObserver(this);
+		this.windowElements = new ArrayList<GameStatusBarElement<Integer>>();
+		
+		for(GameStatusBarElement<Integer> element : elements){
+			windowElements.add(element);
+		}
+	}
+	
+	public void newLevel(Integer newLevel){
+		this.windowElements.get(0).getElement().setValue(newLevel);
+		this.windowElements.get(0).update();
 	}
 
 	@Override
 	public void start() {
+		int currentLevel = 1;
 		for (GameLevel level : data.getLevels()) {
 			data.getEndOfGame().setValue(false);
 			if (currentPlayedLevel != null && currentPlayedLevel.isAlive()) {
 				currentPlayedLevel.interrupt();
 				currentPlayedLevel = null;
 			}
-			currentPlayedLevel = (SokobanLevel) level;
+			currentPlayedLevel = (GameLevelDefaultImpl) level;
+			newLevel(currentLevel);
+			currentLevel ++;
 			currentPlayedLevel.start();
 			try {
 				currentPlayedLevel.join();
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,11 @@ package main;
 
 import game.SokobanConfiguration;
 import game.SokobanGame;
+import gameframework.base.ObservableValue;
 import gameframework.game.Game;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
+import gameframework.gui.GameStatusBarElement;
 import gameframework.gui.GameWindow;
 import level.EndLevel;
 import level.Level1;
@@ -12,6 +14,7 @@ import level.Level2;
 import level.Level3;
 import level.Level4;
 import level.Level5;
+import level.Level6;
 
 /**
  * Creates the game with the levels.
@@ -24,15 +27,20 @@ public class Main {
 	public static void main(String[] args) {
 		GameConfiguration gameConfiguration = new SokobanConfiguration();
 		GameData gamedata = new GameData(gameConfiguration);
-		Game game = new SokobanGame(gamedata);
+		
+		GameStatusBarElement<Integer> level = new GameStatusBarElement<Integer>("Level:", new ObservableValue<Integer>(1));
+		
+		Game game = new SokobanGame(gamedata, level);
 
 		GameWindow gameWindow = new GameWindow("SokoBlob (by DREAMTEAM)",
-				gamedata.getCanvas(), gameConfiguration);
+				gamedata.getCanvas(), gameConfiguration, level);
+		
 		gamedata.addLevel(new Level1(gamedata));
 		gamedata.addLevel(new Level2(gamedata));
 		gamedata.addLevel(new Level3(gamedata));
 		gamedata.addLevel(new Level4(gamedata));
 		gamedata.addLevel(new Level5(gamedata));
+		gamedata.addLevel(new Level6(gamedata));
 		gamedata.addLevel(new EndLevel(gamedata));
 
 		gameWindow.createGUI();
